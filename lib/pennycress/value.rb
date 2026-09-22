eff481be4908775d8a79a443113d66059904a578
Justin Locsei <justin.locsei@gmail.com>
2026-09-22 10:42:45 -0400

    Add scaffolding for a value

--- lib/pennycress/value.rb
+++ lib/pennycress/value.rb
@@ -1,6 +1,31 @@
 # frozen_string_literal: true
 
+require_relative "value_config"
+
 module Pennycress
+  # A value describes a computation performed on a set of inputs.  After the
+  # initial value is computed, it is cached and reused until an invalidation
+  # condition is met.
   class Value
+    class << self
+      # Defines the inputs from which the value is derived
+      #
+      # @param models [Array<Symbol>] model IDs (e.g., `:uploaded_file, :user`)
+      # @param other [Hash{Symbol => Class}] other input types (e.g., `name: String`)
+      # @return [void]
+      def derived_from(*model_ids, **other)
+        inputs = config.inputs
+
+        inputs.model_ids = model_ids
+        inputs.named = other
+      end
+
+    private
+
+      # @return [ValueConfig] configuration for the value
+      def config
+        @config ||= ValueConfig.new
+      end
+    end
   end
 end
--- spec/pennycress/value_spec.rb
+++ spec/pennycress/value_spec.rb
@@ -3,4 +3,23 @@
 require "pennycress/value"
 
 RSpec.describe Pennycress::Value do
+  describe ".derived_from" do
+    it "supports model IDs" do
+      Class.new(Pennycress::Value) do
+        derived_from :uploaded_file, :user
+      end
+    end
+
+    it "supports model IDs and named inputs" do
+      Class.new(Pennycress::Value) do
+        derived_from :uploaded_file, :user, name: String
+      end
+    end
+
+    it "supports only named inputs" do
+      Class.new(Pennycress::Value) do
+        derived_from name: String
+      end
+    end
+  end
 end
