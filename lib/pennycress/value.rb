1614b8eb35eb69b883779d20b1d274a5821331ed
Justin Locsei <justin.locsei@gmail.com>
2026-09-23 07:50:28 -0400

    Wire up fetch mechanics

--- lib/pennycress/value.rb
+++ lib/pennycress/value.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative "constraints"
 require_relative "input_set"
 require_relative "value_config"
 
@@ -8,6 +9,8 @@ module Pennycress
   # initial value is computed, it is cached and reused until an invalidation
   # condition is met.
   class Value
+    include Constraints
+
     class << self
       # Defines the inputs from which the value is derived
       #
@@ -18,12 +21,40 @@ module Pennycress
         config.inputs = InputSet.new(model_ids: model_ids, named: other)
       end
 
+      # Calculates an ouptut value for the given inputs
+      #
+      # @param inputs [Object]
+      # @return [Object] the output value
+      # @raise [ValidationError] if the inputs are invalid
+      def fetch(**inputs)
+        new.send(:fetch, **config.inputs.validate(inputs))
+      end
+
     private
 
       # @return [ValueConfig] configuration for the value
       def config
         @config ||= ValueConfig.new
       end
+    end
+
+    # Calculates an output value for valid inputs
+    #
+    # @param inputs [Object]
+    # @return [Object] the output value
+    # @api value
+    def derive(*)
+      require_method(:derive)
+    end
+
+  private
+
+    # Calculates an output value for valid inputs
+    #
+    # @param inputs [Object]
+    # @return [Object] the output value
+    def fetch(**inputs)
+      derive(**inputs)
     end
   end
 end
--- spec/pennycress/value_spec.rb
+++ spec/pennycress/value_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require "pennycress/errors"
 require "pennycress/value"
 
 RSpec.describe Pennycress::Value do
@@ -20,6 +21,38 @@ RSpec.describe Pennycress::Value do
       Class.new(Pennycress::Value) do
         derived_from name: String
       end
+    end
+  end
+
+  describe ".fetch" do
+    let(:doubled_value) do
+      Class.new(Pennycress::Value) do
+        derived_from id: Integer
+
+        def derive(id:)
+          id * 2
+        end
+      end
+    end
+
+    it "raises when derive is not implemented" do
+      value_class = Class.new(Pennycress::Value) do
+        derived_from id: Integer
+      end
+
+      expect { value_class.fetch(id: 1) }.to raise_error(NotImplementedError)
+    end
+
+    it "raises when inputs are invalid" do
+      expect { doubled_value.fetch(id: "100") }.to raise_error(
+        Pennycress::ValidationError,
+        /100/
+      )
+    end
+
+    it "returns derive results for valid inputs" do
+      expect(doubled_value.fetch(id: 3)).to eq(6)
+      expect(doubled_value.fetch(id: 5)).to eq(10)
     end
   end
 end
