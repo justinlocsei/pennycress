8e8bbfaf80bd02d1aba617a43c208594ccdce413
Justin Locsei <justin.locsei@gmail.com>
2026-09-23 09:50:12 -0400

    Tighten validation for value configs

--- lib/pennycress/value_config.rb
+++ lib/pennycress/value_config.rb
@@ -1,17 +1,35 @@
 # frozen_string_literal: true
 
+require_relative "errors"
 require_relative "input_set"
+require_relative "output"
 
 module Pennycress
-  # A value config holds data describing how a value behaves.  This includes
-  # inputs, invalidation conditions, and partitions.
+  # A value config holds data describing how a value behaves.
   class ValueConfig
-    # @return [InputSet] the inputs for the value
-    attr_accessor :inputs
+    # @!attribute [w] inputs
+    #   @param value [InputSet]
+    attr_writer :inputs
 
-    # Creates a value config
-    def initialize
-      @inputs = InputSet.new
+    # @!attribute [w] output
+    #   @param value [Output]
+    attr_writer :output
+
+    # @return [InputSet] the inputs for the value
+    # @raise [ValidationError] if inputs are not present
+    def inputs
+      raise ValidationError, "inputs are not defined" if @inputs.nil?
+      raise ValidationError, "inputs are empty" if @inputs.empty?
+
+      @inputs
+    end
+
+    # @return [Output] the output for the value
+    # @raise [ValidationError] if output is not defined
+    def output
+      raise ValidationError, "output is not defined" if @output.nil?
+
+      @output
     end
   end
 end
--- spec/pennycress/value_config_spec.rb
+++ spec/pennycress/value_config_spec.rb
@@ -1,19 +1,29 @@
 # frozen_string_literal: true
 
+require "pennycress/errors"
 require "pennycress/value_config"
 
 RSpec.describe Pennycress::ValueConfig do
   let(:config) { Pennycress::ValueConfig.new }
 
   describe "#inputs" do
-    it "is empty by default" do
-      inputs = config.inputs
-
-      expect(inputs.model_ids).to be_empty
-      expect(inputs.named).to be_empty
+    it "raises when inputs are not defined" do
+      expect { config.inputs }.to raise_error(
+        Pennycress::ValidationError,
+        "inputs are not defined"
+      )
     end
 
-    it "can be configured" do
+    it "raises when inputs are empty" do
+      config.inputs = Pennycress::InputSet.new
+
+      expect { config.inputs }.to raise_error(
+        Pennycress::ValidationError,
+        "inputs are empty"
+      )
+    end
+
+    it "returns configured inputs" do
       config.inputs = Pennycress::InputSet.new(
         model_ids: %i[user post],
         named: { id: Integer, name: String }
@@ -21,6 +31,21 @@ RSpec.describe Pennycress::ValueConfig do
 
       expect(config.inputs.model_ids).to eq(%i[post user])
       expect(config.inputs.named).to eq({ id: Integer, name: String })
+    end
+  end
+
+  describe "#output" do
+    it "raises when output is not defined" do
+      expect { config.output }.to raise_error(
+        Pennycress::ValidationError,
+        "output is not defined"
+      )
+    end
+
+    it "returns configured output" do
+      config.output = Pennycress::Output.new(Integer)
+
+      expect(config.output.validate(1)).to eq(1)
     end
   end
 end
