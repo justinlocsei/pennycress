73b95f3e616cfd868ac863d38d18bdcf83c586c0
Justin Locsei <justin.locsei@gmail.com>
2026-09-25 19:45:14 -0400

    Use an isolated registry for every test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
+require "pennycress/registry"
 require "support/pennycress/test_helpers"
 
 RSpec.configure do |config|
   config.disable_monkey_patching!
   config.include Pennycress::TestHelpers
+
+  config.around do |example|
+    Pennycress::Registry.override(Pennycress::Registry.new) { example.run }
+  end
 end
