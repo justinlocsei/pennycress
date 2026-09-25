cb017315e7d7e85ac26ad044002d79ebf13afdfa
Justin Locsei <justin.locsei@gmail.com>
2026-09-25 07:26:45 -0400

    Add a top-level configure method

--- lib/pennycress.rb
+++ lib/pennycress.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
+require_relative "pennycress/configuration"
 require_relative "pennycress/version"
-require_relative "pennycress/value"
+
+# Pennycress allows Rails apps to precompute and cache expensive logic, with
+# fine-grained invalidation and support for targeted cache warming.
+module Pennycress
+  class << self
+    # Configures Pennycress
+    #
+    # @yieldparam config [Configuration] the current configuration
+    # @return [void]
+    def configure
+      yield Configuration.current
+    end
+  end
+end
--- spec/pennycress_spec.rb
+++ spec/pennycress_spec.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
 require "pennycress"
+require "pennycress/configuration"
 
 RSpec.describe Pennycress do
   it "exposes a version" do
     expect(Pennycress::VERSION).to be_a(String)
+  end
+
+  describe ".configure" do
+    it "yields the current configuration" do
+      described_class.configure do |config|
+        expect(config).to equal(Pennycress::Configuration.current)
+      end
+    end
   end
 end
